be294c39e60d1484d4a03746562756d091d0315d
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-04 13:19:47 +0500

    Javascript practice

---
+++ JS_course/main.js
@@ -0,0 +1,15 @@
+// console.dir(document)
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.all);
+// console.log(document.all[10]);
+
+// console.log(document.getElementById('hedaer-title'))
+var hedaerTitle = (document.getElementById('hedaer-title'))
+console.log(hedaerTitle)
+// hedaerTitle.textContent = 'Hello';
+// hedaerTitle.innerText = 'Goodbye';
+hedaerTitle.style.border = 'Hello';
